a808295e63421b2db91530e9a85d27d0cf6a5486
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-02-03 13:57:56 +1300

    Initial version of queued payload classes

---
+++ TTNAPIApplicationToAzureIoTDeviceClient/Model/TTNDownlinkQueuedPayload.cs
@@ -0,0 +1,58 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.TheThingsNetwork.Models
+{
+   using System.Collections.Generic;
+   using System.Runtime.Serialization;
+
+   using Newtonsoft.Json;
+   using Newtonsoft.Json.Linq;
+
+   public class DownlinkQueued
+   {
+      [JsonProperty("f_port")]
+      public int Port { get; set; }
+
+      [JsonProperty("frm_payload")]
+      public string PayloadRaw { get; set; }
+
+      [JsonProperty("decoded_payload")]
+      public JToken PayloadDecoded { get; set; }
+
+      [JsonProperty("Priority")]
+      public string priority { get; set; }
+
+      [JsonProperty("confirmed")]
+      public bool Confirmed { get; set; }
+
+      [JsonProperty("correlation_ids")]
+      public List<string> CorrelationIds { get; set; }
+   }
+
+   public class DownlinkQueuedPayload
+   {
+      [JsonProperty("end_device_ids")]
+      public EndDeviceIds EndDeviceIds { get; set; }
+      [JsonProperty("correlation_ids")]
+      public List<string> CorrelationIds { get; set; }
+      [JsonProperty("downlink_queued")]
+      public DownlinkQueued DownlinkQueued { get; set; }
+   }
+}
+
+
+
---
+++ TTNAPIApplicationToAzureIoTDeviceClient/Model/TTNDownlinkQueuedPayloadRaw.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.TheThingsNetwork.Models.DownlinkQueuedRaw
+{
+   using System.Collections.Generic;
+
+   // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+   public class ApplicationIds
+   {
+      public string application_id { get; set; }
+   }
+
+   public class EndDeviceIds
+   {
+      public string device_id { get; set; }
+      public ApplicationIds application_ids { get; set; }
+   }
+
+   public class DownlinkQueued
+   {
+      public int f_port { get; set; }
+      public string frm_payload { get; set; }
+      public string priority { get; set; }
+      public List<string> correlation_ids { get; set; }
+   }
+
+   public class Root
+   {
+      public EndDeviceIds end_device_ids { get; set; }
+      public List<string> correlation_ids { get; set; }
+      public DownlinkQueued downlink_queued { get; set; }
+   }
+}
