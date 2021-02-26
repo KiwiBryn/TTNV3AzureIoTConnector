5c81b220f747699fafc617a18a96cec39f400be7
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-02-26 13:33:22 +1300

    Fixed up the year in (c) notice

--- TheThingsIndustriesAzureIoTConnector/Program.cs
+++ TheThingsIndustriesAzureIoTConnector/Program.cs
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------------
-// Copyright (c) February 2020, devMobile Software
+// Copyright (c) February 2021, devMobile Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,35 +16,35 @@
 //---------------------------------------------------------------------------------
 namespace devMobile.TheThingsIndustries.TheThingsIndustriesAzureIoTConnector
 {
-   using System;
+	using System;
 
-   using Microsoft.Extensions.DependencyInjection;
-   using Microsoft.Extensions.Hosting;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
 
-   using Microsoft.Extensions.Logging;
-   using Microsoft.Extensions.Options;
+	using Microsoft.Extensions.Logging;
+	using Microsoft.Extensions.Options;
 
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         CreateHostBuilder(args).Build().Run();
-      }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-      public static IHostBuilder CreateHostBuilder(string[] args) =>
-         Host.CreateDefaultBuilder(args)
-         .ConfigureLogging(logging =>
-         {
-            logging.AddApplicationInsights();
-            logging.ClearProviders();
-            logging.AddConsole();
-         })
-            .ConfigureServices((hostContext, services) =>
-         {
-            services.Configure<ProgramSettings>(hostContext.Configuration.GetSection("ProgramSettings"))
-               .AddHostedService<Worker>()
-               .AddApplicationInsightsTelemetryWorkerService()
-               .AddTransient<ProgramSettings>(a => a.GetRequiredService<IOptions<ProgramSettings>>().Value);
-         });
-   }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			.ConfigureLogging(logging =>
+			{
+				logging.AddApplicationInsights();
+				logging.ClearProviders();
+				logging.AddConsole();
+			})
+				.ConfigureServices((hostContext, services) =>
+			{
+				services.Configure<ProgramSettings>(hostContext.Configuration.GetSection("ProgramSettings"))
+					.AddHostedService<Worker>()
+					.AddApplicationInsightsTelemetryWorkerService()
+					.AddTransient<ProgramSettings>(a => a.GetRequiredService<IOptions<ProgramSettings>>().Value);
+			});
+	}
 }
